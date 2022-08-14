bf48ef8ee38771cc030e8c95bb6789b721c13490
Sam Hickey <52939970+samuelhickeyIII@users.noreply.github.com>
2022-08-14 00:40:41 -0400

    Perceptron updated

--- Neural-Network/Components/Perceptron.py
+++ Neural-Network/Components/Perceptron.py
@@ -1,25 +1,44 @@
-from cmath import exp
+import numpy as np
+from scipy import rand
 
 
 class Perceptron(object):
     def __init__(self, **kwargs):
         """
         Parameters:
-        - features: List(float)
-        - weights: List(float) 
-        - bias: float
-        - activation: function
+        - features:     numpy.array
+        - weights:      numpy.array of shape self.features
+        - bias:         numpy.float
+        - activation:   function
+        - input_layer:  boolean
         """
         
-        self.features = kwargs.get("features", [])
-        self.weights = kwargs.get("weights", [1 for _ in self.features])
+        self.features = kwargs.get("features", np.empty((1,1)))
+        self.weights = kwargs.get("weights", np.random.rand(*self.features.shape))
         self.bias = kwargs.get("bias", 0)
         self.activation = kwargs.get("activation", None)
+        self.input_layer = kwargs.get("input_layer", False)
         self.output = None
 
-    def activate(self) -> None:
-        dot_prod = self.bias \
-                    + sum([f * w for f,w in zip(self.features, self.weights)])
+    def value(self):
+        v = self.bias \
+            + np.sum(np.multiply(self.features, self.weights))
 
+        if self.activation == "input":
+            self.output = v
         if self.activation == "sigmoid":
-            self.output = 1.0 / (1.0 + exp(-dot_prod))
+            self.output = 1.0 / (1.0 + np.exp(-v))
+        return self.output
+
+def main():
+    mlr = Perceptron(
+        features=np.random.rand(1,5),
+        weights=np.random.rand(1,5),
+        bias=0,
+        activation="sigmoid"
+    )
+
+    print(mlr.value())
+
+if __name__ == "__main__":
+    main()
