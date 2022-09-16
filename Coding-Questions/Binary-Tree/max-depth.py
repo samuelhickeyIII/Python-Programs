bc859c7f8d0da204f4e14e56ba760f41f4df43e1
Sam Hickey <samuelahickey@gmail.com>
2022-09-16 10:12:06 -0400

    Coding-Questions init

---
+++ Coding-Questions/Binary-Tree/max-depth.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        elif not root.left and not root.right:
+            return 1
+        else:
+            return max(maxDepth(root.left)+1, maxDepth(root.right)+1)
