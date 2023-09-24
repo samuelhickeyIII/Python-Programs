75ad3db1c2627dd1b4b65321a5dd2f80cb1af8d4
Sam Hickey <samuelahickey@gmail.com>
2023-09-24 16:11:05 -0400

    Uncomment create statement

--- Chess/worksheets/sql/setup/create-database.sql
+++ Chess/worksheets/sql/setup/create-database.sql
@@ -1,7 +1,7 @@
--- CREATE DATABASE chess_analytics
---   CHARACTER SET = 'utf8mb4'
---   COLLATE = 'utf8mb4_general_ci';
+CREATE DATABASE chess_analytics
+  CHARACTER SET = 'utf8mb4'
+  COLLATE = 'utf8mb4_general_ci';
 
-alter database chess_analytics
-  character set = 'utf8mb4'
-  collate = 'utf8mb4_general_ci';
+-- alter database chess_analytics
+--   character set = 'utf8mb4'
+--   collate = 'utf8mb4_general_ci';
