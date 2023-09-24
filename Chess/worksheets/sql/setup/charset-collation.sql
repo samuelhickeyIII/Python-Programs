71993671a515eed115e95cea213025ab62d4b3f9
Sam Hickey <samuelahickey@gmail.com>
2023-09-24 16:12:01 -0400

    refactor

--- Chess/worksheets/sql/setup/charset-collation.sql
+++ Chess/worksheets/sql/setup/charset-collation.sql
@@ -1,10 +1,8 @@
 -- InnoDB Syntax
 alter table staging character set = 'utf8mb4';
-
 -- alter table staging COLLATE = 'utf8mb4_general_ci';
 
 
 -- ColumnStore Syntax:
 -- alter table games character set 'utf8mb4';
-
 -- alter table staging COLLATE = 'utf8mb4_general_ci';
